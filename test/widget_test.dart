8bd2bd3ccbdcf689dbf7e5e3d6ef32a1a5f8bb0d
sab1103b <est.santiagoa.bola1@unimilitar.edu.co>
2024-11-16 11:47:47 -0500

    movimiento main dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bild/FRONT/main.dart';
+import 'package:bild/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
