ab275c1d458df57208a2999f921fe3447f6e7e5d
sab1103b <est.santiagoa.bola1@unimilitar.edu.co>
2024-11-16 15:40:55 -0500

    back

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bild/screens/login.dart';
 import 'package:bild/screens/perfil.dart';
 import 'package:bild/screens/fyp.dart';
 
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
- runApp(const MainApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
