2615fc7f963ca40a8a154ac973da971e2e228e16
sab1103b <est.santiagoa.bola1@unimilitar.edu.co>
2024-11-16 21:56:05 -0500

    arreglo

--- lib/screens/PERFIL.dart
+++ lib/screens/PERFIL.dart
@@ -45,119 +45,123 @@ class _PerfilScreenState extends State<Perfil> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          // Sección de fondo con gradiente y contenido del perfil
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [vinotintoOscuro, beigeClaro],
-              ),
-            ),
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // Imagen de perfil
-                Center(
-                  child: CircleAvatar(
-                    radius: 80.0,
-                    backgroundImage: AssetImage(_profileImage),
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Sección de fondo con gradiente y contenido del perfil
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [vinotintoOscuro, beigeClaro],
                 ),
-                const SizedBox(height: 16.0),
-
-                // Nickname
-                _isEditing
-                    ? TextField(
-                        controller: _nicknameController,
-                        decoration: InputDecoration(
-                          labelText: 'Nickname',
-                          prefixIcon: Icon(Icons.tag_faces, color: vinotintoOscuro),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                      )
-                    : Text(
-                        _nickname,
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          color: beigeClaro,
-                        ),
-                      ),
-                const SizedBox(height: 16.0),
-
-                // Botón de acción: Editar perfil / Guardar cambios
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _isEditing = !_isEditing; // Cambiar entre modo edición y no edición
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: beigeClaro,
-                    padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 10.0),
-                  ),
-                  child: Text(
-                    _isEditing ? 'Guardar cambios' : 'Editar perfil',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: vinotintoOscuro,
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  // Imagen de perfil
+                  Center(
+                    child: CircleAvatar(
+                      radius: 80.0,
+                      backgroundImage: AssetImage(_profileImage),
                     ),
                   ),
-                ),
-                const SizedBox(height: 24.0),
-              ],
-            ),
-          ),
-          
-          // Sección de fotos subidas debajo del fondo rojo
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: _userPhotos.isNotEmpty
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Fotos subidas',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: beigeClaro,
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      GridView.builder(
-                        shrinkWrap: true,
-                        physics: NeverScrollableScrollPhysics(),
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 8.0,
-                          mainAxisSpacing: 8.0,
-                        ),
-                        itemCount: _userPhotos.length,
-                        itemBuilder: (context, index) {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              _userPhotos[index],
-                              fit: BoxFit.cover,
+                  const SizedBox(height: 16.0),
+
+                  // Nickname
+                  _isEditing
+                      ? TextField(
+                          controller: _nicknameController,
+                          decoration: InputDecoration(
+                            labelText: 'Nickname',
+                            prefixIcon: Icon(Icons.tag_faces, color: vinotintoOscuro),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
                             ),
-                          );
-                        },
+                          ),
+                        )
+                      : Text(
+                          _nickname,
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: beigeClaro,
+                          ),
+                        ),
+                  const SizedBox(height: 16.0),
+
+                  // Botón de acción: Editar perfil / Guardar cambios
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _isEditing = !_isEditing; // Cambiar entre modo edición y no edición
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: beigeClaro,
+                      padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 10.0),
+                    ),
+                    child: Text(
+                      _isEditing ? 'Guardar cambios' : 'Editar perfil',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: vinotintoOscuro,
                       ),
-                    ],
-                  )
-                : Text(
-                    'No has subido fotos aún.',
-                    style: TextStyle(color: beigeClaro),
+                    ),
                   ),
-          ),
-        ],
+                  const SizedBox(height: 24.0),
+                ],
+              ),
+            ),
+            
+            // Sección de fotos subidas debajo del fondo rojo
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: _userPhotos.isNotEmpty
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Fotos subidas',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: beigeClaro,
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        GridView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 8.0,
+                            mainAxisSpacing: 8.0,
+                          ),
+                          itemCount: _userPhotos.length,
+                          itemBuilder: (context, index) {
+                            return ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                _userPhotos[index],
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    )
+                  : Text(
+                      'No has subido fotos aún.',
+                      style: TextStyle(color: beigeClaro),
+                    ),
+            ),
+          ],
+        ),
       ),
+      
+      // Barra inferior de navegación
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
         child: Row(
